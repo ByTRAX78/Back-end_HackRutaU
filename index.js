f03d4810375ca09138fa6c5d3474036794ed930f
Isain Cuadra <isain.cuadra@konfio.mx>
2024-11-10 10:19:55 -0600

    fix: headers

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ exports.handler = async (event, context) => {
     result = {"anals": {}, "patronesMovilidad": {}}
 
     return {
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'POST,GET,OPTIONS',
+            'Content-Type': 'application/json'
+        },
         statusCode: 200,
         body: JSON.stringify(result)
     };
